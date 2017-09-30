2ab77b2b430047fa4a30695531fc63a304592f48
David Thrasher <thrasher1332@hotmail.com>
2017-09-30 13:40:56 -0400

    Materialize CDN and First Component Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Nav from './components/Nav.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Nav />
       </div>
     );
   }
