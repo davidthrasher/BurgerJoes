fd79640cbce5d7717908af4966800952b7890087
David Thrasher <thrasher1332@hotmail.com>
2017-10-05 20:43:27 -0400

    changed file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Nav from './components/Nav.js';
+import Nav from './components/Nav/Nav.js';
 
 class App extends Component {
   render() {
