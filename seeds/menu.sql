20c3a21522b8d8d51517424bfaac66604a20ab55
Harrison McCord <harrison.mccord@gmail.com>
2017-10-13 15:42:17 -0400

    starting adding menu items

---
+++ seeds/menu.sql
@@ -0,0 +1,8 @@
+INSERT INTO menu (menuType, menuItem, menuPrice)
+VALUES ("burgers", "Joe's Original", 5.89), ("burgers", "Joe's Original Cheese", 6.39), ("burgers", "Joe's Bacon Cheese", 6.79),
+("burgers", "Joe's Blue Cheese Mushroom", 6.59), ("burgers", "Joe's Mushroom Swiss", 6.39), ("burgers", "Joe's BBQ Bacon Cheese", 6.79),
+("burgers", "Joe's Caribbean Jerk", 6.39), ("burgers", "Joe's One Way Philly Cheese", 6.59), ("burgers", "Joe's Favorite Way", 6.59);
+
+INSERT INTO menu (menuType, menuItem, menuPrice)
+VALUES ("chicken", "Joe's Original Chicken Sandwich", 6.29), ("chicken", "BBQ Chicken Sandwich", 6.49), 
+("chicken", "Teriyaki Chicken Sandwich", 6.49), ("chicken", "Chicken Club Sandwich", 6.79);
