747e5fe6c20bffd39ccbaf6940d7bfc0e7e1c64a
David Thrasher <thrasher1332@hotmail.com>
2017-10-12 21:23:00 -0400

    Changed login button class and end point

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -8,13 +8,13 @@ const Navbar = () =>
           <a href="/"><img src="./images/BurgerJoesLogo.jpg" className="brand-logo" alt="Burger Joes" /></a>
           <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
           <ul className="right hide-on-med-and-down">
-            <li><a href="/login" className="navfont">Login/Sign Up</a></li>
+            <li><a className="loginbutton" className="navfont">Login/Sign Up</a></li>
             <li><a href="/menu" className="navfont">Menu</a></li>
             <li><a href="/locations" className="navfont">Locations</a></li>
             <li><a href="/burgerversity" className="navfont">Burgerversity</a></li>
           </ul>
           <ul className="side-nav" id="mobile-demo">
-            <li><a href="/login">Login/Sign Up</a></li>
+            <li><a className="loginbutton">Login/Sign Up</a></li>
             <li><a href="/menu">Menu</a></li>
             <li><a href="/locations">Locations</a></li>
             <li><a href="/burgerversity">Burgerversity</a></li>
