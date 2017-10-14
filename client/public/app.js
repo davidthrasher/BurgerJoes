cf93fd451c8604d9b4365cd5f1b62867617cd9e9
David Thrasher <thrasher1332@hotmail.com>
2017-10-14 13:26:25 -0400

    Added maps

--- client/public/app.js
+++ client/public/app.js
@@ -2,4 +2,13 @@ $( document ).ready(function(){
 
   $(".button-collapse").sideNav();
 
+  initMap();
+  function initMap() {
+          var latlng = new google.maps.LatLng(0, 0);
+          var map = new google.maps.Map(document.getElementById('google'), {
+            zoom: 2,
+            center: latlng,
+            mapTypeId: "hybrid"
+          });
+  };
 });
