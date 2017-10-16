d5d2434400ffea9d8a4600b8e43285684db3c8c3
David Thrasher <thrasher1332@hotmail.com>
2017-10-16 11:51:28 -0400

    Transparent Cards

--- client/src/components/Careers/Careers.js
+++ client/src/components/Careers/Careers.js
@@ -7,7 +7,7 @@ const Careers = () =>
   <h1 className="center white-text">Careers</h1>
   <div className="row">
     <div className="col s12 m12 l12">
-      <div id="applycard" className="card red darken-4 center">
+      <div id="applycard" className="card transparent center">
         <div className="white-text center">
           Come join a fun, fast paced and rewarding team at Burger Joe's! We're always looking for hard working, driven individuals who come to work ready to serve our customers with a smile! If this sounds like you, apply today!
         </div>
@@ -17,12 +17,12 @@ const Careers = () =>
   </div>
   <div className="row">
     <div className="col s12 m12 l6">
-      <div className="card red darken-4 center">
+      <div className="card transparent center">
         <img src="./images/sourceimages/storefront.jpg" className="careers" alt="Burger Joes"/>
       </div>
     </div>
     <div className="col s12 m12 l6">
-      <div className="card red darken-4 center">
+      <div className="card transparent center">
         <img src="./images/sourceimages/apparel.jpg" className="careers" alt="Burger Joes"/>
       </div>
     </div>
--- client/src/components/Story/Story.js
+++ client/src/components/Story/Story.js
@@ -6,17 +6,17 @@ const Story = () =>
   <h1 className="center white-text">Our Story</h1>
   <div className="row">
     <div className="col s12 m12 l6">
-      <div className="card red darken-4 center">
+      <div className="card transparent center">
         <img src="./images/sourceimages/storyscan.jpg" className="ourstory" alt="Burger Joes"/>
       </div>
     </div>
     <div className="col s12 m12 l6">
-      <div className="card red darken-4 center">
+      <div className="card transparent center">
         <img src="./images/sourceimages/handshake.jpg" className="handshake" alt="Burger Joes"/>
       </div>
     </div>
     <div className="col s12 m12 l6">
-      <div className="card red darken-4 center">
+      <div className="card transparent center">
         <img src="./images/sourceimages/togocounter.jpg" className="handshake" alt="Burger Joes"/>
       </div>
     </div>
