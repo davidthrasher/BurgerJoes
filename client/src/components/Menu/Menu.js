f3e54e3588adf8439619ae230c54dae877abaa7c
Harrison McCord <harrison.mccord@gmail.com>
2017-10-16 12:21:20 -0400

    menu controller

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -3,6 +3,11 @@ import React from "react";
 const Menu = () =>
 
 <main>
-	
+	<h1 className="header center white-text">Menu</h1>
+
+	<div className="col s12 m12">
+		
+	</div>
+
 </main>
 export default Menu;
