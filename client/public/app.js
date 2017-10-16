d568f9d5985b30c4c63102081078c2740a53bdb9
Ryan <rkhardeman@gmail.com>
2017-10-16 19:32:41 -0400

    google maps, not functional

--- client/public/app.js
+++ client/public/app.js
@@ -2,6 +2,20 @@ $( document ).ready(function(){
 
   $(".button-collapse").sideNav();
 
-
+  //call google maps function on document ready
+  initMap();
 
 });
+
+//google maps
+function initMap() {
+  var latlong = {lat: 33.7742969, lng: -84.4063173};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: latlong
+  });
+  var marker = new google.maps.Marker({
+    position: latlong,
+    map: map
+  });
+}
