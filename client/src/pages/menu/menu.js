6445c87292af0c955764abdcbcdb4d1efa310f03
David Thrasher <thrasher1332@hotmail.com>
2017-10-16 13:14:12 -0400

    Updated Menu

--- client/src/pages/menu/menu.js
+++ client/src/pages/menu/menu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import Nav from '../../components/Navbar/Navbar.js';
 import Footer from '../../components/Footer/Footer.js';
+import MenuMenu from '../../components/Menu/Menu.js';
 
 
 class Menu extends Component {
@@ -10,7 +11,7 @@ class Menu extends Component {
       <div>
 
         <Nav {...this.props}/>
-
+        <MenuMenu />
         <Footer />
       </div>
     );
