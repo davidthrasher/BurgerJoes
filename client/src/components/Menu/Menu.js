15bc46a5068e23eb28bbb74a14454755c2c5cfe9
David Thrasher <thrasher1332@hotmail.com>
2017-10-16 13:10:14 -0400

    Fixed Merge Conflict and updated menu

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -3,10 +3,50 @@ import React from "react";
 const Menu = () =>
 
 <main>
-	<h1 className="header center white-text">Menu</h1>
 
-	<div className="col s12 m12">
-		
+	<h1 className="center white-text">Menu</h1>
+
+	<div className="row">
+		<div className="col s6 offset-m2 m4 l3">
+				<p className="white-text">Burgers</p>
+				<a href="/menu/burgers"><img src="./images/foodpics/burgernail.jpg" className="menuitem" alt="Burger Joes"/></a>
+
+		</div>
+		<div className="col s6 offset-m2 m4 l3">
+				<p className="white-text">Chicken Sandwiches</p>
+				<a href="/menu/chickensandwiches"><img src="./images/foodpics/chickensandwich.jpg" className="menuitem" alt="Burger Joes"/></a>
+
+		</div>
+		<div className="col s6 offset-m2 m4 l3">
+				<p className="white-text">Salads</p>
+				<a href="/menu/salads"><img src="./images/foodpics/salad.jpeg" className="menuitem" alt="Burger Joes"/></a>
+
+		</div>
+		<div className="col s6 offset-m2 m4 l3">
+				<p className="white-text">Tenders</p>
+				<a href="/menu/tenders"><img src="./images/foodpics/tenders.jpg" className="menuitem" alt="Burger Joes"/></a>
+
+		</div>
+		<div className="col s6 offset-m2 m4 l3">
+				<p className="white-text">Wings</p>
+				<a href="/menu/wings"><img src="./images/foodpics/wings.jpg" className="menuitem" alt="Burger Joes"/></a>
+
+		</div>
+		<div className="col s6 offset-m2 m4 l3">
+				<p className="white-text">Sides</p>
+				<a href="/menu/sides"><img src="./images/foodpics/fries.jpg" className="menuitem" alt="Burger Joes"/></a>
+
+		</div>
+		<div className="col s6 offset-m2 m4 l3">
+				<p className="white-text">Desserts</p>
+				<a href="/menu/desserts"><img src="./images/foodpics/dessert.jpg" className="menuitem" alt="Burger Joes"/></a>
+
+		</div>
+		<div className="col s6 offset-m2 m4 l3">
+				<p className="white-text">Drinks</p>
+				<a href="/menu/drinks"><img src="./images/foodpics/drinks.jpg" className="menuitem" alt="Burger Joes"/></a>
+
+		</div>
 	</div>
 
 </main>
