d0ef8a695d16f13877748b34bbd85d4c2a1ff683
Harrison McCord <harrison.mccord@gmail.com>
2017-10-18 12:40:45 -0400

    updating server

--- server.js
+++ server.js
@@ -8,10 +8,23 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+const routes = require("./controllers/menuController.js");
+
+app.use('/', routes);
+
+app.use('/menu', routes);
+app.use('/menu/:id', routes);
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+db.sequelize.sync({force: false}).then(function() {
+	app.listen(PORT, function(err) {
+		if (err) throw err;
+		console.log("Listening on port: " + PORT);
+	});
 });
 
 app.listen(PORT, function() {
