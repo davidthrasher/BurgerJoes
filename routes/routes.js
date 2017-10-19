04a0d5df27d55126030c1e00eb3561f6118440d1
Harrison McCord <harrison.mccord@gmail.com>
2017-10-19 11:25:44 -0400

    working on router

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-
+const router = require("express").Router();
 var db = require("../models");
 
 module.exports = function(app) {
@@ -14,3 +14,4 @@ module.exports = function(app) {
   });
 
 };
+module.exports = router;
